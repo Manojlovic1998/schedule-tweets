4d491845d6fe2e8d13a0d1a7855c8162fa858c02
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-04-30 22:50:46 +0200

    Create action for user create on sign_up POST

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,19 @@ class RegistrationsController < ApplicationController
   def new
     # Instance var, visible in views
     @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
