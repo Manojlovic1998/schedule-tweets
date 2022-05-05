5c181cf1d203189fbc3c04031c01a442a58b8e52
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-05 10:12:11 +0200

    Set twitter_account record using before action

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,5 +1,6 @@
 class TwitterAccountsController < ApplicationController
   before_action :is_logged_in?
+  before_action :set_twitter_account, only: [:destroy]
 
   def index
     @twitter_accounts = Current.user.twitter_accounts
@@ -7,8 +8,13 @@ class TwitterAccountsController < ApplicationController
 
 
   def destroy
-    @twitter_account = Current.user.twitter_accounts.find(params[:id])
     @twitter_account.destroy()
     redirect_to twitter_accounts_path, notice: "Successfully disconnected #{twitter_account.username}."
+  end
+
+  private
+
+  def set_twitter_account
+    @twitter_account = Current.user.twitter_accounts.find(params[:id])
   end
 end
